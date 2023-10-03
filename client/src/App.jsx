dc51e8c1ac7b2f1ed7b029e88ae6863ad1213899
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2023-10-03 13:22:44 +0100

    Add routes to application

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-import { Navbar, Appform } from './components'
+import { Navbar, Appform, Payment } from './components'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="App">
+    <Router>
       <Navbar />
-      <Appform />
+      <Routes>
+        <Route index path="/" element={<Appform />} />
+        <Route path='/payment' element={<Payment />}/>
+      </Routes>
+    </Router>
     </div>
   )
 }
