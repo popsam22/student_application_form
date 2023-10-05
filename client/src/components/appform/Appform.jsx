f73f735df7d6fdffdffca18cb3e7692617f347c0
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2023-10-05 10:16:09 +0100

    Fix: update Application data variable name

--- client/src/components/appform/Appform.jsx
+++ client/src/components/appform/Appform.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 
 function Appform() {
   const navigate = useNavigate() 
-  const [data, setData] = useState({
+  const [FormData, setFormData] = useState({
     name: '', dob:'', gender:'',nationality:'',
     state:'', religion:'', phone:'', email:'',
     disability:'', class:'', prev_school:'',
@@ -15,16 +15,14 @@ function Appform() {
 
 
   const changeValues = (name, value)=> {
-    setData({...data, [name]:value})
+    setFormData({...FormData, [name]:value})
   }
 
   const submitApplication = () => {
     //submit to backend
-    console.log(data, 'dreerdfew fd')
+    console.log(FormData)
     navigate('/payment')
   }
-
-  
 
   return (
     <div className="application-form flex flex-col border-solid border-2 shadow-2xl p-10 mt-32 mb-24">
