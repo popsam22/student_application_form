6d3a9d3eba858badf22ce7681f6e25ca6f0b11e6
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2023-10-06 08:49:30 +0100

    Modify navbar color and logo

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,26 +1,26 @@
 import './navbar.css';
-import { logo, email, facebook, insta, phone, x }  from '../../assets'
+import { logo, email, facebook, insta, phone, x, school_logo }  from '../../assets'
 
 
 function Navbar() {
   return (
-    <div className='fixed top-0 border-b-2 w-[100%] h-24 flex items-center pl-32 bg-slate-100'>
-      <div className='pl-8 mr-32'>
-        <img src={logo} alt='skoolbod logo'/>
+    <div className='fixed top-0 border-b-2 w-[100%] h-24 flex items-center pl-32 bg-[#00c274]'>
+      <div className='pl-10 mr-40 w-40 h-24 pt-2 pb-2'>
+        <img src={school_logo} alt='skoolbod logo' className='w-[100%] h-[100%]'/>
       </div>
       <div className='flex items-center space-x-2 bg-[#00c274] p-2 rounded-full mr-32'>
         <div className=' bg-gray-200 p-2 rounded-lg hover:bg-[#0047ab]'>
           <a href='#facebook'>
-            <img src={facebook} alt='facebook' className='w-3.5'/>
+            <img src={facebook} alt='facebook' className='w-6'/>
           </a>
         </div>
         <div className=' bg-gray-200 p-2 rounded-lg hover:bg-[#0047ab]'>
           <a href='#insta'>
-            <img src={insta} alt='insta' className='w-3.5'/>
+            <img src={insta} alt='insta' className='w-6'/>
           </a>
         </div><div className=' bg-gray-200 p-2 rounded-lg hover:bg-[#0047ab]'>
         <a href='#facebook'>
-          <img src={x} alt='x' className='w-3.5'/>
+          <img src={x} alt='x' className='w-6'/>
         </a>
       </div>
       </div>
